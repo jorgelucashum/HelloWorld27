27412a59e216ae51d99c4d00af24ab759bd74133
Jorge Lucas <78880067+jorgelucashum@users.noreply.github.com>
2021-03-31 15:34:50 -0300

    Update OrderStatus.cs

--- HelloWorld27/Entities/Enums/OrderStatus.cs
+++ HelloWorld27/Entities/Enums/OrderStatus.cs
@@ -1,6 +1,10 @@
 ﻿
 namespace HelloWorld27.Entities.Enums
 {
+    // Enumerações (Enum):
+    // - É um tipo especial que serve para especificar de forma literal um conjunto de constantes relacionadas.
+    // - Melhor semântica, é do tipo valor, código mais legível e auxiliado pelo compilador.
+    
     enum OrderStatus : int // OrderStatus está no tipo 'enum'. // ': int' especifica que os valores do enum são inteiros.
     {
         PeendingPayment = 0,
