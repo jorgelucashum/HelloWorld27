98f82c4896aa6b3cec4214496298236b68b1b3af
Jorge Lucas <78880067+jorgelucashum@users.noreply.github.com>
2021-03-31 15:30:54 -0300

    Update Program.cs

--- HelloWorld27/Program.cs
+++ HelloWorld27/Program.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 namespace HelloWorld27
 {
     // Enumerações (Enum):
-    // - É um tipo especial que serve para especificar d eforma literal um conjunto de constantes relacionadas.
-    // - Melhor semântica, é do tipo valor, código mais legível, e auxiliado pelo compilador.
+    // - É um tipo especial que serve para especificar de forma literal um conjunto de constantes relacionadas.
+    // - Melhor semântica, é do tipo valor, código mais legível e auxiliado pelo compilador.
     class Program
     {
         static void Main(string[] args)
